b5565d95e5ac7ca8601d9e1348e05673ee924791
Arthur Balejo <146049033+arthurbalejo@users.noreply.github.com>
2024-01-21 02:41:08 -0300

    update

--- curso_em_video/funcoes/ex_98.py
+++ curso_em_video/funcoes/ex_98.py
@@ -1,5 +1,21 @@
 #faça um programa que tenha uma função chamada contador(), que receba três parâmetros: inicio, fim e passo e realize a contagem.
 #seu programa tem que realizar tres contagens atraves da função criada
 #a) de 1 ate 10, de 1 em 1
-#b) de até 0, de 2 em 2
+#b) de 10 até 0, de 2 em 2
 #c) uma contagem personalizada
+
+def contador(inicio, fim, passo):
+    if inicio >= fim:
+        while inicio > fim:
+            print(inicio)
+            inicio -= passo
+    if inicio <= fim:
+        while inicio <= fim:
+            print(inicio)
+            inicio += passo
+
+i = 10
+f = 1
+p = 1
+
+contador(i, f, p)
