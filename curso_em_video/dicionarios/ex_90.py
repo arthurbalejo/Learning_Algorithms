7234917fc245fca128db1bc2ff52e0df0666e1b1
Arthur Balejo <146049033+arthurbalejo@users.noreply.github.com>
2024-02-16 20:43:31 -0300

    update

--- curso_em_video/dicionarios/ex_90.py
+++ curso_em_video/dicionarios/ex_90.py
@@ -1,1 +1,13 @@
 #faca um programa que leia nome e media de um aluno, guardando tambem a situacao em um dicionario. no final, mostre o conteudo da estrutura na tela.
+
+aluno = dict()
+
+aluno['nome'] = str(input('insira o nome: '))
+aluno['media'] = int(input('insira a media: '))
+if aluno['media'] > 7:
+    aluno['situacao'] = 'bom'
+else:
+    aluno['situacao'] = 'ruim'
+
+for c, v in aluno.items():
+    print(f'{c}: {v}')
--- curso_em_video/dicionarios/ex_91.py
+++ curso_em_video/dicionarios/ex_91.py
@@ -1,1 +1,23 @@
 #crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatorios. guarde esses resultados em um dicionario. no final, coloqueesse dicionario em ordem,, sabendo que o vencedor tirou o maior numero.
+import random
+from time import sleep
+from operator import itemgetter
+
+jogo = {
+    'jogador1': random.randint(1,6),
+    'jogador2': random.randint(1,6),
+    'jogador3': random.randint(1,6),
+    'jogador4': random.randint(1,6),
+}
+
+for k, v in jogo.items():
+    print(f"o {k} tirou {v} no dados.")
+    sleep(1)
+rankign = list()
+rankign = sorted(jogo.items(), key=itemgetter(1), reverse=True)
+print(rankign)
+# for i, v in enumerate(rankign):
+#     print(f'{i} lugar {v[0]} com {v[1]}')
+#     sleep(1)
+for i in range(len(rankign)):
+    print(f'{i} lugar {rankign[i][0]} com {rankign[i][1]}')
